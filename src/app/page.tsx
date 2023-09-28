841fe428f70d374b83d30449812dced1963b9e55
MolinDeng <zjumorning@gmail.com>
2023-09-28 17:25:08 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
                 width={1419}
                 height={732}
                 quality={100}
-                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10 "
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
               />
             </div>
           </div>
