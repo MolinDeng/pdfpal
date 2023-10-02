b95d8f969967acac286d93c4b085624f5b202c0d
MolinDeng <zjumorning@gmail.com>
2023-10-01 21:30:50 -0400

    tRPC test

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,16 @@
-import { router } from './trpc';
-const appRouter = router({
-  // ...
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { publicProcedure, router } from './trpc';
+import { TRPCError } from '@trpc/server';
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+    if (!user.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
+
+    // check if user is in the database
+
+    return { success: true, user: { id: user.id, email: user.email } };
+  }),
 });
 // Export type router type signature,
 // NOT the router itself.
