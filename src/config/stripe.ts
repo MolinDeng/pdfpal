0d0c721ac7a2f1b2dd39f91bce81eb5fe9410a5a
MolinDeng <zjumorning@gmail.com>
2023-10-20 15:58:11 -0400

    saas part

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -20,7 +20,7 @@ export const PLANS = [
     price: {
       amount: 14,
       priceIds: {
-        test: 'price_1NuEwTA19umTXGu8MeS3hN8L',
+        test: 'price_1O3OdzAfDDO34NcvZ7CUsQXk',
         production: '',
       },
     },
