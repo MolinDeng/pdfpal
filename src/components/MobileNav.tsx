d78fdc4a98162471f7f0b1b3d22323b7f1b34a63
MolinDeng <zjumorning@gmail.com>
2023-10-21 12:17:14 -0400

    add useEffect depen

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -14,7 +14,7 @@ const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
 
   useEffect(() => {
     if (isOpen) toggleOpen();
-  }, [pathname]);
+  }, [pathname, isOpen]);
 
   const closeOnCurrent = (href: string) => {
     if (pathname === href) {
